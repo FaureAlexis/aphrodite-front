5000a49da65bcfa753fb263447fd50ed29ecef4d
Alexis Faure <alexis.faure@epitech.eu>
2022-12-02 00:12:58 +0100

    [UPDATE] - Update app.vue to display navbar and router view

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="flex min-h-screen w-full">
+    <NavBar></NavBar>
+    <main class="flex min-h-screen flex-1 flex-col rounded-l-[48px]">
+      <router-view/>
+    </main>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavBar from './components/NavBar.vue';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default defineComponent({
+  name: 'App',
+  components: {
+    NavBar,
+  },
+});
+</script>
