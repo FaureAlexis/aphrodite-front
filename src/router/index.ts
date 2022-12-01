9f984cbed831e3cd361b9d463d203cb8550fc55b
Alexis Faure <alexis.faure@epitech.eu>
2022-12-01 21:34:16 +0100

    [ADD] - Add Quiz vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    component: () => import(/* webpackChunkName: "Quiz" */ '../views/Quiz.vue'),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/Quiz.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="component-root">
+    QUIZ
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Quiz',
+});
+</script>
+
+<style>
+
+</style>
