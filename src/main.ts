2f519f49f25c46d17b008c52b649af3a35db1266
Alexis Faure <alexis.faure@epitech.eu>
2022-12-02 00:12:20 +0100

    [Add] - Add font awesome icons

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,25 @@
 /* eslint-disable import/no-unresolved */
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-//import {  } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome, faQuestion, faQuoteLeft, faMessage,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router/index';
+import titleMixin from './mixins/titleMixin';
 import store from './store/index';
 import './assets/reset.css';
 
-library.add();
+library.add(faHome, faQuestion, faQuoteLeft, faMessage);
 createApp({
   extends: App,
   beforeCreate() { this.$store.commit('initialiseStore'); },
-}).use(store).use(router).component('fa', FontAwesomeIcon)
+})
+  .use(store)
+  .use(router)
+  .component('fa', FontAwesomeIcon)
+  .mixin(titleMixin)
   .mount('#app');
 store.subscribe((mutation, state) => {
   localStorage.setItem('store-aphrodite', JSON.stringify(state));
