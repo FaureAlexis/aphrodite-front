b73cd748db24cb5c82a92345eba2a66a72dfb056
Alexis Faure <alexis.faure@epitech.eu>
2022-12-02 04:14:08 +0100

    [UI] - Apply footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <NavBar></NavBar>
     <main class="flex min-h-screen flex-1 flex-col rounded-l-[48px]">
       <router-view/>
+      <Footer></Footer>
     </main>
   </div>
 </template>
@@ -10,11 +11,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    NavBar,
+    NavBar, Footer,
   },
 });
 </script>
