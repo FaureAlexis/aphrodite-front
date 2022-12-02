f16bf8ce0c5c015acedbd637b634f2c49e62bf28
Alexis Faure <alexis.faure@epitech.eu>
2022-12-02 07:31:33 +0100

    [FRONT] - Set back url

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    BaseUrl: 'http://localhost:3000',
+    BaseUrl: 'https://aphrodite-back.vercel.app',
     isConnected: false,
   },
   mutations: {
