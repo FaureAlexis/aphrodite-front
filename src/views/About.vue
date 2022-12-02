7e9fb798bc0f67c3539ed4c654c48294deb6e4de
Alexis Faure <alexis.faure@epitech.eu>
2022-12-02 05:58:31 +0100

    [ADD] - Add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div  class="about bg-hero_color min-h-screen flex justify-center items-center">
+    <div class="flex flex-col">
+      <span class="text-4xl font-extrabold text-transparent
+        bg-clip-text bg-gradient-to-r to-primary from-purple-700">Aphrodite
+      </span>
+      <h2 class="text-2xl text-left text-text font-bold">Un projet d'Hackathon</h2>
+      <span class="">Projet visant à sensibiler les jeunes (et les - jeunes !)<br>
+        aux risques liés au VIH et aux autre infections sexuellement transmissibles.</span>
+    </div>
   </div>
 </template>
