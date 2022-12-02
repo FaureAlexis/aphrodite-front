70b1f120d3495499839315376f122e07f635a608
Alexis Faure <alexis.faure@epitech.eu>
2022-12-02 04:13:21 +0100

    [ADD] - Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="card-root" class="bg-background border-2 border-white rounded-10 p-3 shadow-xl
+  min-h-200 bg-line">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Card',
+});
+</script>
+
+<style>
+
+</style>
